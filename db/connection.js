365302ca92da43968e000d9528a8753b513109e0
aa57c <aa57c@umsystem.edu>
2025-04-28 17:13:17 -0500

    edited connection details

--- db/connection.js
+++ db/connection.js
@@ -1,35 +1,31 @@
-// Module connection to mongoDB database.
+// Module connection to MongoDB database.
 
 const { MongoClient } = require("mongodb");
-const Db = process.env.MONGO_URI;
-const client = new MongoClient(Db, {
+require('dotenv').config();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
- 
+
 // Our database connection
 let dbConnection;
 
 module.exports = {
-    //Establishes the connection to the mongodb server.
-    connectToServer: (callback) =>
-    {
-        client.connect(function (err,db)
-        {
-            if(err || !db) 
-            {
-                return callback(err);
-            }
-
-            dbConnection = db.db('mydb');
-            console.log('Succesfully connected to Mongodb');
-            return callback();
-        })
-    },
-
-    //Returns the connection
-    getDB: () =>
-    {
-        return dbConnection;
+  // Establishes the connection to the MongoDB server.
+  connectToServer: async (callback) => {
+    try {
+      await client.connect();
+      dbConnection = client.db('mydb'); // <-- YOUR database name here
+      console.log('Successfully connected to MongoDB');
+      callback();
+    } catch (err) {
+      console.error('Failed to connect to MongoDB', err);
+      callback(err);
     }
+  },
+
+  // Returns the connection
+  getDB: () => dbConnection,
 };
